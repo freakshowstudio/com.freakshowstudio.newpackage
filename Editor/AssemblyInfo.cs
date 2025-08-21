de6d3632b8b4c0c7ba4146f44186cfb097f61c66
Stig Olavsen <stig.olavsen@gmail.com>
2025-08-21 14:16:15 +0200

    fix: Update and include assembly metadata

---
+++ Editor/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿
+using System.Reflection;
+
+[assembly:AssemblyTitle("NewPackage Editor")]
+[assembly:AssemblyCompany("Freakshow Studio AS")]
+[assembly:AssemblyCopyright("(C) 2019 Freakshow Studio AS")]
